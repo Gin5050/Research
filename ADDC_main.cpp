288f1eb08b317380876f5478436810c4523b388e
後藤勇輝 <gotouyuuki@gotouyuuki-no-MacBook-Air.local>
2018-05-07 08:56:03 +0900

    今日やったこと Re_Be_funcの作成．
    次やること
    Re_Be_funcでのビーコンの受信処理．
    ビーコンは受信時間でしか定義されてないので，具体的なデータレートかビーコンのサイズが必要．
    
    今後やりたいこと
    重複処理の抽出．Process関数をmain部分に移行．

--- ADDC_main.cpp
+++ ADDC_main.cpp
@@ -2,13 +2,15 @@
 #include "system.h"
 #include "channel.h"
 //#include "degree_define.h"
-//#include "awgn.h"
+#include "awgn.h"
 
 void Initialaize_nodes(NODE *n_data, int N);
 void Process_node(NODE *n_data, int N, double t_count, double p_sleep, list<int> &Beacon_node, list<int> &ACK_node, list<int> &Trans_node, list<int> &All_trans);
-void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_node);
-void Re_Be_func(NODE *n_data, double t_count, int num, int N, list<int> &Beacon_node);
+void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_node, list<int> &All_trans);
+void Re_Be_func(NODE *n_data, double t_count, int num, int N, list<int> &Beacon_node, list<int> &All_trans);
 int MinBack(NODE *n_data, int num, int mode, int N, list<int> &Beacon_node);
+double SINR(NODE *n_data, double t_count, int num, int min_node, int mode, list<int> &All_trans);
+void DBPSK(NODE *n_data, double sinr, int num, int temp);
 
 int main(){
   int i, j;
@@ -40,13 +42,17 @@ int main(){
       Initialaize_nodes(n_data, N);
       t_count = 0;
       while(t_count < OBSERVE){
+	// for(i = 0; i < N; i++){
+	//   n_data[i].mode = n_data[i].next_mode;
+	//   n_data[i].state = n_data[i].next_state;
+	// }
+	//if(int(t_count) % 5 == 0 && t_count > 1) cout << t_count << endl;
 	Process_node(n_data, N, t_count, p_sleep, Beacon_node, ACK_node, Trans_node, All_trans);
 	//if(int(t_count) % 10 == 0 && t_count > 1) cout << t_count << endl;
 	t_count += TCOUNT;
       }
     }
   }
-  
   return 0;
 }
 
@@ -64,13 +70,13 @@ void Process_node(NODE *n_data, int N, double t_count, double p_sleep, list<int>
 
   for(i = 0; i < N; i++){
     switch (n_data[i].mode){
-
-      // case SLEEP:
-      // 	// if(abs(t_count - n_data[i].activetime) < TCOUNT){
-      // 	//   n_data[i].Sleep_Change_Mode(t_count, p_sleep);
-      // 	// }
-      // 	n_data[i].sleep_time+=TCOUNT;
-      // 	break;
+      
+      case SLEEP:
+      	if(abs(t_count - n_data[i].activetime) < TCOUNT){
+      	  n_data[i].Sleep_Change_Mode(t_count, p_sleep);	
+      	}
+      	n_data[i].sleep_time+=TCOUNT;
+      	break;
 
       // case CSMA:
       // 	CSMA_CA(n_data, N, i, t_count, Beacon_node);
@@ -144,27 +150,28 @@ void Process_node(NODE *n_data, int N, double t_count, double p_sleep, list<int>
 }
 
 //CSMA/CA
-void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_node){
+void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_node, list<int> &All_trans){
   int i,j;
   double dis_node = 0;
   double r_level = 0;
   double *p_dist;
   complex<double> channel_coeff;
-  
-  for(i = 0; i < N; i++){ //他端末の送信をチェック
-    if(num != i){
-      if(n_data[i].mode == TRANSMIT || n_data[i].mode == BEACON || n_data[i].mode == Be_ACK){
-	dis_node = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));
-	channel_coeff = rayleigh();
-	r_level = Pathloss(dis_node, PATHLOSS_num) + abs(channel_coeff) * abs(channel_coeff);
-	if(r_level > CA_dBm){
-	  break; //CAレベルを超えていれば
-	}
+  list<int> ::iterator it;  
+
+  it = All_trans.begin();
+  while(it != All_trans.end()){
+    if(num != *it){
+      dis_node = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));
+      channel_coeff = rayleigh();
+      r_level = Pathloss(dis_node, PATHLOSS_num) + abs(channel_coeff) * abs(channel_coeff);
+      if(r_level > CA_dBm){
+	break; //CAレベルを超えていれば
       }
     }
+    ++it;
   }
   
-  if(i == N){
+  if(it != All_trans.end()){
     if(n_data[num].ca_time > 0) n_data[num].ca_time = n_data[num].ca_time - TCOUNT;
     else if(n_data[num].ca_time < TCOUNT){
        switch (n_data[num].state){
@@ -172,6 +179,7 @@ void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_nod
   	  n_data[num].next_mode = BEACON;
   	  n_data[num].be_end = t_count + BEACON_TIME; //Beacon送信時間は1ms
 	  Beacon_node.push_back(num);
+	  All_trans.push_back(num);
   	  break;	  
   	// case SENDER:
   	//   if(n_data[num].s_end < t_count){ //CSMAなどによりsender終了時刻を飛び越した場合
@@ -198,17 +206,17 @@ void CSMA_CA(NODE *n_data, int N, int num, double t_count, list<int> &Beacon_nod
 }
 
 //SenderからのACK待ち
-void Re_Be_func(NODE *n_data, double t_count, int num, int N, list<int> &Beacon_node){
+void Re_Be_func(NODE *n_data, double t_count, int num, int N, list<int> &Beacon_node, list<int> &All_trans){
    int temp = -1;
    int flag = 0;
    double sinr = 0;
    uniform_int_distribution<> randCW(0, CW-1);
  
    temp = MinBack(n_data, num, Be_ACK, N, Beacon_node);
-  if(temp != -1){
-    //sinr = SINR(n_data, t_count, num, Be_ACK, p_data);
-    //flag = n_data[temp].flag;
-    //DBPSK(n_data, p_data, sinr, num, temp);
+   if(temp != -1){
+     sinr = SINR(n_data, t_count, num, temp, Be_ACK, All_trans);
+     DBPSK(n_data, sinr, num, temp);
+     
     //n_data[num].rx_time+=TCOUNT;
     // if(n_data[num].rec_cnt == SHORTSIZE && n_data[num].timestamp[flag] < n_data[temp].timestamp[flag]){
     //   n_data[num].next_mode = SLEEP;
@@ -225,66 +233,101 @@ void Re_Be_func(NODE *n_data, double t_count, int num, int N, list<int> &Beacon_
   //else n_data[num].idle_time+=TCOUNT;
 }
 
-// double SINR(NODE *n_data, double t_count, int num, int mode, Parameters *p_data){
-//   int i;
-//   int nummode = n_data[num].mode;
-//   int temp = -1;
-//   double distance = 0;
-//   double min = 100000;
-//   double interference = 0;
-//   double loss = 0;
-//   double sinr = 0;
-//   complex<double> x;
-//   double fading = 0;
+double SINR(NODE *n_data, double t_count, int num, int min_node, int mode, list<int> &All_trans){
+  int i;
+  int nummode = n_data[num].mode;
+  int temp = -1;
+  double distance = 0;
+  double min = 100000;
+  double interference = 0;
+  double loss = 0;
+  double sinr = 0;
+  complex<double> x;
+  double fading = 0;
+  list<int> ::iterator it;
 
-//   //calculate minimum distance
-//   for(i = 0; i < N; i++){
-//     if(i != num){
-//       if(n_data[i].mode == mode){
-// 	distance = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));
-// 	if(min > distance){
-// 	  min = distance;
-// 	  temp = i;
-// 	}
-//       }
-//     }
-//   }
+  //calculate minimum distance
+  it = All_trans.begin();
+  while(it != All_trans.end()){
+    if(num != *it){
+      if(n_data[*it].mode == mode){
+	distance = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));
+	if(min > distance){
+	  min = distance;
+	  temp = *it;
+	}
+      }     
+    }
+    ++it;
+  }
 
-//   //calculate SINR
-//   if(temp != -1){
-//     for(i = 0; i < N; i++){
-//       if(num != i && temp != i){
-// 	if(n_data[i].mode == TRANSMIT || n_data[i].mode == BEACON ||
-// 	   n_data[i].mode == Be_ACK || n_data[i].mode == COLLISION_MG){
-// 	  distance = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));
-// 	  //distance = abs(n_data[num].node_location - n_data[i].node_location);
-// 	  loss = Pathloss(distance, PATHLOSS_num);
-// 	  x = channel[num][i];
-// 	  fading = 10 * log10(abs(x) * abs(x));
-// 	  interference += (pow(10, (fading + loss) / 10.0));
-// 	  if(nummode != RECEIVE &&  nummode != SENDER)
-// 	    Collision_check_sensor(n_data, p_data, t_count, num, i);
-// 	}
-//       }
-//     }
-//     loss = Pathloss(min, PATHLOSS_num);
-//     x = channel[num][temp];
-//     fading = 10 * log10(abs(x) * abs(x));
-//     sinr = loss + fading - (10 * log10(interference + NOISE));
-//     //cout << sinr << "\t" << temp << "\t" << num << "\t" << min << "\t" << interference << "\t" << fading << endl;
-//   }
-//   return sinr;
-// }
+  //calculate SINR
+  if(temp != -1){
+    it = All_trans.begin();
+    while(it != All_trans.end()){
+      if(num != *it && temp != *it){
+	distance = sqrt(pow(n_data[num].x - n_data[i].x, 2) + pow(n_data[num].y - n_data[i].y, 2));	
+	loss = Pathloss(distance, PATHLOSS_num);
+	x = rayleigh();
+	fading = 10 * log10(abs(x) * abs(x));
+	interference += (pow(10, (fading + loss) / 10.0));
+	//if(nummode != RECEIVE &&  nummode != SENDER)
+	  //Collision_check_sensor(n_data, p_data, t_count, num, i);	
+      }
+    }
+    loss = Pathloss(min, PATHLOSS_num);
+    x = rayleigh();;
+    fading = 10 * log10(abs(x) * abs(x));
+    sinr = loss + fading - (10 * log10(interference + NOISE));
+    //cout << sinr << "\t" << temp << "\t" << num << "\t" << min << "\t" << interference << "\t" << fading << endl;
+  }
+  return sinr;
+}
 
-int MinBack(NODE *n_data, int num, int mode, int N, list<int> &Beacon_node){
+void DBPSK(NODE *n_data, double sinr, int num, int temp){
+
+  int i,j;
+  complex<double> x;  
+  double p_d = 0;
+  complex<double> m_temp;
+  
+  if(n_data[num].rec_time == 0){
+    x = rayleigh();
+    n_data[num].fading = x;
+    n_data[num].sinr = sinr;
+    n_data[num].connect = temp;
+    
+    //1シンボル遅延
+    m_temp = complex<double>(1.0,0);
+    m_temp = m_temp * x;
+    m_temp += complex<double>(awgnQ(sinr), awgnI(sinr));
+    n_data[num].m_prev = m_temp;
+  }
+  n_data[num].rec_time++;
+  
+  //DBPSK 1us分の受信
+  for(j = 0; j < DATARATE * TCOUNT; j++){
+    m_temp = complex<double>(1.0,0);
+    m_temp = m_temp * n_data[num].fading;
+    m_temp += complex<double>(awgnQ(sinr), awgnI(sinr));     
+    p_d = abs(arg(n_data[num].m_prev) - arg(m_temp));
+    n_data[num].m_prev = m_temp;
+    if(cos(p_d) < 0){ //1bitでも誤った場合は終了
+      break;
+    }
+  }
+  n_data[num].rec_cnt += j;
+}
+
+int MinBack(NODE *n_data, int num, int mode, int N, list<int> &nodes){
   int i;
   int temp = -1;
   double distance = 0;
   double min = 1000000;
   list<int> ::iterator it;
 
-  it = Beacon_node.begin();
-  while(it != Beacon_node.end()){
+  it = nodes.begin();
+  while(it != nodes.end()){
     if(*it != num){
       if(n_data[*it].mode == mode){
 	distance = sqrt(pow(n_data[num].x - n_data[*it].x, 2) + pow(n_data[num].y - n_data[*it].y, 2));
