684268154ba75f1f24d7713252c37e919f3a444e
Gin5050 <gotou9640@gmail.com>
2018-07-15 21:51:07 +0900

    Calculatorからclacutilに関数移動

--- CalcUtil.hpp
+++ CalcUtil.hpp
@@ -17,7 +17,7 @@ class CalcUtile{
     int min = 10000000;
     int nodeId = EMPTY;
 
-    if(nodes.empty() == TRUE) return EMPTY;
+    //if(nodes.empty() == TRUE) return EMPTY;
     /*他端末からの送信信号の受信電力を測定*/
     it = nodes.begin();
     while(it != nodes.end()){
@@ -29,6 +29,112 @@ class CalcUtile{
       it++;
     }
     return nodeId;
+  }
+
+  static int CarrierSense(NODE *n_data, int id, ModeMemory *modeMemo){
+    list<int> ::iterator it;
+    double distance = 0;
+    double CSLevel;
+    complex<double> channel_coeff;
+      
+    /*他端末からの送信信号の受信電力を測定*/
+    it = modeMemo->All_trans.begin();
+    //cout << modeMemo->getAllTransNodes().size() << endl; 
+    while(it != modeMemo->All_trans.end()){
+      if(id != *it){
+	distance = CalcUtile::NodesDistance(n_data[id].x, n_data[id].y, n_data[*it].x, n_data[*it].y);
+	channel_coeff = Channel::rayleigh();
+	CSLevel = Channel::pathLoss(distance, PATHLOSS_num) + abs(channel_coeff) * abs(channel_coeff);
+	if(CSLevel > CA_dBm){	  
+	  break; //CAレベルを超えていれば
+	}
+      }      
+      ++it;
+    }    
+
+    if(it != modeMemo->All_trans.end()){      
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+  static int searchBeacon(double x, double y, NODE *n_data, ModeMemory *modeMemo){
+    int nodeId = CalcUtile::MinNode(x, y, modeMemo->Beacon_node, n_data);
+
+   if(nodeId == EMPTY){
+      return EMPTY;
+    }
+    return nodeId;
+  }
+
+  static int searchAck(double x, double y, NODE *n_data, ModeMemory *modeMemo){
+    int nodeId = CalcUtile::MinNode(x, y, modeMemo->ACK_node, n_data);
+
+    if(nodeId == EMPTY){
+      return EMPTY;
+    }
+    return nodeId;
+  }
+
+  static int searchTx(double x, double y, NODE *n_data, ModeMemory *modeMemo){
+    int nodeId = EMPTY;
+    nodeId += modeMemo->Trans_node.size();
+      //CalcUtile::MinNode(x, y, modeMemo->Trans_node, n_data);
+
+    if(nodeId == EMPTY){
+      return EMPTY;
+    }
+    return nodeId;
+  }
+
+  static double calcSinr(NODE *n_data, double x, double y, int minNode, ModeMemory *modeMemo){
+    list<int> ::iterator it = modeMemo->All_trans.begin();
+    complex<double> chnlCoeff;
+    double distance = 0;
+    double loss = 0;
+    double fading = 0;
+    double interference = 0;    
+    //int min = CalcUtile::MinNode(n_data, id, All_trans);
+    
+    while(it != modeMemo->All_trans.end()){
+      if(*it != minNode){
+	distance = CalcUtile::NodesDistance(x, y, n_data[*it].x, n_data[*it].y);
+	loss = Channel::pathLoss(distance, PATHLOSS_num);
+	chnlCoeff = Channel::rayleigh();
+	fading = 10 * log10(abs(chnlCoeff) * abs(chnlCoeff));
+	interference += (pow(10, (fading + loss) / 10.0));
+      }
+      it++;
+    }
+    distance = CalcUtile::NodesDistance(x, y, n_data[minNode].x, n_data[minNode].y);
+    loss = Channel::pathLoss(distance, PATHLOSS_num);
+    chnlCoeff = Channel::rayleigh();;
+    fading = 10 * log10(abs(chnlCoeff) * abs(chnlCoeff));
+
+    return (loss + fading - (10 * log10(interference + NOISE)));
+  }
+
+  static double calcSinrJakes(NODE *n_data, double x, double y, int minNode, ModeMemory *modeMemo, double t_count){
+    list<int> ::iterator it = modeMemo->All_trans.begin();
+    complex<double> chnlCoeff;
+    double distance = 0;
+    double loss = 0;
+    double fading = 0;
+    double interference = 0;    
+    
+    while(it != modeMemo->All_trans.end()){
+      if(*it != minNode){
+	distance = CalcUtile::NodesDistance(x, y, n_data[*it].x, n_data[*it].y);
+	loss = Channel::pathLoss(distance, PATHLOSS_num);
+	chnlCoeff = n_data[*it].jakes(t_count);
+	fading = 10 * log10(abs(chnlCoeff) * abs(chnlCoeff));
+	interference += (pow(10, (fading + loss) / 10.0));
+      }
+      it++;
+    }
+    distance = CalcUtile::NodesDistance(x, y, n_data[minNode].x, n_data[minNode].y);
+    loss = Channel::pathLoss(distance, PATHLOSS_num);  
+    return (loss - (10 * log10(interference + NOISE)));
   }
 
   static complex<double> getConsteration(NODE *n_data, double sinr, int txId, double t_count){
